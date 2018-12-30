3cd0da7ffa61d22440a3054729c391a06536119a
song <2986715422@qq.com>
2018-12-30 15:00:39 +0800

    Update index.py

--- index.py
+++ index.py
@@ -12,4 +12,4 @@ def index():
     return render_template('index.html')
 if __name__ == '__main__':
     from route import *
-    app.run(host='0.0.0.0',port=port,debug = True)
+    app.run(host='0.0.0.0',port=port,debug = False)
